3724c31ff8338ab04bd29b0024bebcd8a9444222
Warintorn Wiboonsin <warintorn.wiboonsild@gmail.com>
2021-07-29 16:40:24 +0700

    refactor code

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -1,17 +1,22 @@
 const Event = require("../../models/event");
 const User = require("../../models/user");
 const bcrypt = require("bcryptjs");
+const Booking = require("../../models/booking");
+
+const transformEvent = event => {
+  return {
+    ...event._doc,
+    _id: event.id,
+    date: new Date(event._doc.date).toISOString(),
+    creator: user.bind(this, event.creator),
+  };
+};
 
 const events = async (eventIds) => {
   try {
     const events = await Event.find({ _id: { $in: eventIds } });
     events.map((event) => {
-      return {
-        ...event._doc,
-        _id: event.id,
-        date: new Date(event._doc.date).toISOString(),
-        creator: user.bind(this, event.creator),
-      };
+      return transformEvent(events);
     });
 
     return events;
